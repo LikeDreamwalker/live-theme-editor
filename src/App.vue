1ab44cfaa2761a0ebe1811d609883afcc918ce7c
ldw <a@ldwid.com>
2021-03-05 17:35:26 +0800

    feat(all): start
    start

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <router-view></router-view>
+  </v-app>
+</template>
+
+<script></script>
+<style>
+/* 解决页面左右溢出 */
+html,
+body {
+  max-width: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+</style>
